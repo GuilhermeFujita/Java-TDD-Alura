027843d6a2a3768cba2431b28bca30c3819bfc1b
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-06-17 16:20:03 -0300

    Finished function to get 3 values

--- teste-de-unidade/src/br/com/caelum/leilao/teste/AvaliadorTest.java
+++ teste-de-unidade/src/br/com/caelum/leilao/teste/AvaliadorTest.java
@@ -1,13 +1,15 @@
 package br.com.caelum.leilao.teste;
 
-import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
 
 import br.com.caelum.leilao.dominio.Lance;
 import br.com.caelum.leilao.dominio.Leilao;
 import br.com.caelum.leilao.dominio.Usuario;
 import br.com.caelum.leilao.servico.Avaliador;
-import junit.framework.Assert;
 
 public class AvaliadorTest {
 
@@ -71,5 +73,28 @@ public class AvaliadorTest {
 		
 		assertEquals(1000.0, leiloeiro.getMaiorLance(), 0.00001);
 		assertEquals(1000.0, leiloeiro.getMenorLance(), 0.00001);
+	}
+	
+	@Test
+	public void deveEncontrarOsTresMaioresLances() {
+		Usuario joao = new Usuario("João");
+		Usuario maria = new Usuario("Maria");
+		Leilao leilao = new Leilao("Playstation 3 novo");
+		
+		leilao.propoe(new Lance(joao, 100.0));
+		leilao.propoe(new Lance(maria, 200.0));
+		leilao.propoe(new Lance(joao, 300.0));
+		leilao.propoe(new Lance(maria, 400.0));
+		
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+		
+		List<Lance> maiores = leiloeiro.getTresMaiores();
+		
+		assertEquals(3, maiores.size());
+		assertEquals(400, maiores.get(0).getValor(), 0.00001);
+        assertEquals(300, maiores.get(1).getValor(), 0.00001);
+        assertEquals(200, maiores.get(2).getValor(), 0.00001);
+		
 	}
 }
