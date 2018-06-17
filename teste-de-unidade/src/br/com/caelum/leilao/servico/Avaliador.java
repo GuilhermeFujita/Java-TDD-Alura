30f9c2289d329491a78e15688747e68c5a32dcbb
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-06-17 16:20:28 -0300

    Bug fix at function

--- teste-de-unidade/src/br/com/caelum/leilao/servico/Avaliador.java
+++ teste-de-unidade/src/br/com/caelum/leilao/servico/Avaliador.java
@@ -46,7 +46,7 @@ public class Avaliador {
 				return 0;
 			}
 		});
-		maiores = maiores.subList(0, 3);
+		 maiores = maiores.subList(0, maiores.size() > 3 ? 3 : maiores.size());
 	}
 	
 	public List<Lance> getTresMaiores(){
